014df991fa42db9ab92770a99443f9c9a44b7eb7
Victor Assis <victor_assis@poli.ufrj.br>
2024-12-18 23:04:30 -0300

    feat: :sparkles: Implements handling to create the output directory if it does not exist

--- src/utils/write_csv_file.py
+++ src/utils/write_csv_file.py
@@ -1,6 +1,10 @@
 import csv
+import os
 
 def write_csv_file(file_path, data):
+  # Cria o diretório se ele não existir
+  os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
   with open(file_path, 'w', newline='') as file:
     fieldnames = data[0].keys() if data else []
     
